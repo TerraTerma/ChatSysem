d21bcc8086ef72e3cda06ff9cb0d2efd9078dfb2
Tre Logan <tr3.logan@gmail.com>
2016-05-21 15:03:09 -0400

    add check for multiverse

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,31 @@
+import java.util.logging.Logger;
+
 import org.bukkit.ChatColor;
+import org.bukkit.Server;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 
 	public static final String VERSION = "1.0";
 	
+	private Logger logger = getLogger();
+	
 	@Override
 	public void onEnable () {
-		getLogger().info(getName() + " " + VERSION + " enabled.");
+
+		boolean hasMultiverse = checkMultiverse();
+		if (!hasMultiverse) logger.info("Couldn't find multiverse.");
+		
+		logger.info(getName() + " " + VERSION + " enabled.");
 	}
 	
 	@Override
 	public void onDisable () {
-		getLogger().info(getName() + " " + VERSION + " disabled.");
+		logger.info(getName() + " " + VERSION + " disabled.");
 	}
 	
 	@Override
@@ -25,6 +36,14 @@ public class Main extends JavaPlugin {
 		}
 		
 		return false;
+	}
+	
+	private boolean checkMultiverse () {
+		final String name = "Multiverse";
+		Server server = getServer();
+		PluginManager pluginManager = server.getPluginManager();
+		Plugin plugin = pluginManager.getPlugin(name);
+		return (plugin != null && plugin.isEnabled());
 	}
 	
 	public String getHelpText () {
